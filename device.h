c9c76927ce6a50c219c10a011105187dc7fa2025
Quackattack <bhavin7009@gmail.com>
2015-09-07 20:06:26 -0400

    Update device.h

--- device.h
+++ device.h
@@ -8,3 +8,19 @@
 
 #define IS_IPAD \
    ([[UIScreen mainScreen] bounds].size.width == 768 || [[UIScreen mainScreen] bounds].size.height == 768)
+
+NSString* machineName()
+{
+    static NSString* name = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        struct utsname systemInfo;
+        uname(&systemInfo);
+        name = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    });
+    return name;
+}
+Then define a few macros as needed:
+
+#define IS_IPHONE_6      [machineName() isEqualToString:@"iPhone7,2"]
+#define IS_IPHONE_6_PLUS [machineName() isEqualToString:@"iPhone7,1"]
